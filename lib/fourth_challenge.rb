bbbefd8e55f7f05f9b035c98d5ceed7de359e128
Chance Smith <chancesmith97@hotmail.com>
2019-02-23 02:14:26 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -13,9 +13,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: ,
-        age: ,
-        attitude: 
+        {
+          name: "Benbolio",
+          age: "17",
+          attitude: "worried"}
+          {name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"}
       ]
    }
 }
